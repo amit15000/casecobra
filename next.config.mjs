5ba1f063df9a625f5e1f96e103c20123d90c9dd4
Amit Kumar <15000amitkumar@gmail.com>
2025-07-17 03:12:32 +0530

    Upload route

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['utfs.io'],
+    domains: ["utfs.io"],
   },
-}
+  experimental: {
+    dynamicApiRoute: "force-dynamic",
+  },
+};
 
-export default nextConfig
+export default nextConfig;
